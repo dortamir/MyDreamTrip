423bcb2b6b4b6f9d4fb9d158fbebda7321a0e990
YuvalKotlovsky <yuvalkat@gmail.com>
2026-01-26 17:47:05 +0200

    fix: redirect to auth when user not logged in

--- app/src/main/java/com/example/mydreamtrip/MainActivity.kt
+++ app/src/main/java/com/example/mydreamtrip/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mydreamtrip
 
 import android.os.Bundle
+import android.content.Intent
+import com.google.firebase.auth.FirebaseAuth
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -11,6 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user == null) {
+            val intent = Intent(this, AuthActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            return
+        }
 
         val navHost = supportFragmentManager.findFragmentById(R.id.mainNavHost) as NavHostFragment
         val navController = navHost.navController
