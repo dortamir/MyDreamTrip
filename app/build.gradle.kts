e65a70e08bf2a9bac4f4e5f23d936f55ca1b1f3f
YuvalKotlovsky <yuvalkat@gmail.com>
2026-01-27 14:52:13 +0200

    Work In Progress

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,5 +58,7 @@ dependencies {
     ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.paging:paging-runtime-ktx:3.3.2")
     implementation("com.squareup.picasso:picasso:2.8")
-
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-al")
 }
